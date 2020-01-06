2ce3d385bfcd7859504ec1a09f93abb61a2a7b07
Antonio Anzures <joaz@gft.com>
2020-01-06 10:59:14 -0600

    ejemplo, modificacion para importar en un solo archivo

--- src/app/personas/personas.component.ts
+++ src/app/personas/personas.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-personas',
-  templateUrl: './personas.component.html'
+  template: `<h1>Listado de personas</h1>
+  <app-persona></app-persona>
+  <app-persona></app-persona>`
 })
 export class PersonasComponent{
 
