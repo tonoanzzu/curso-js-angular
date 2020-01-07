c245cb2943fbbc1ab215384d758515495950c009
Antonio Anzures <joaz@gft.com>
2020-01-07 12:20:40 -0600

    declaracion de properties y funciones, Conceptos basicos de interpolacion, binding, two way binding

--- src/app/personas/personas.component.ts
+++ src/app/personas/personas.component.ts
@@ -3,11 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-personas',
   templateUrl: './personas.component.html',
-  styleUrls:[
-    './personas.component.css'
-  ]
+
+  styles: [` 
+  h1{
+    color:blue;
+  }`]
   
 })
 export class PersonasComponent{
+  agregarPersona = false;
+  agregarPersonaStatus = "No se ha agregado ninguna persona"
+  tituloPersona = "Ingeniero";
 
+  constructor(){
+    setTimeout( ()=>{
+      this.agregarPersona = true
+    }, 3000)
+  }
+
+  onCrearPersona(){
+    this.agregarPersonaStatus = "Persona creada"
+  }
+
+  onModificarPersona(event: Event){
+    this.tituloPersona = (<HTMLInputElement>event.target).value;
+  }
 }
